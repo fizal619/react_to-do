9cd63c2b763b9c2ce55ff14f10610b1bbcd828b7
Fizal Sarif <fizal.sarif@gmail.com>
2016-07-14 14:30:03 -0400

    db schema seed

---
+++ db/seed.sql
@@ -0,0 +1,3 @@
+insert into tasks(task_name,task_desc) values('Call mom', 'Yo call your mom tomorrow!');
+insert into tasks(task_name,task_desc) values('do hq', 'do your hw after you feed the cat');
+insert into tasks(task_name) values('Study React');
